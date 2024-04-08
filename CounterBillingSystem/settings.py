a4ecec41fd0d3860bd946745d0b9dc9b6a95820f
Harshith1263 <101880547+Harshith1263@users.noreply.github.com>
2024-04-09 02:24:44 +0530

    Static folder changed

--- CounterBillingSystem/settings.py
+++ CounterBillingSystem/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_yasg',
+    "whitenoise.runserver_nostatic",
 ]
 
 SWAGGER_SETTINGS = {
@@ -142,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR + "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
